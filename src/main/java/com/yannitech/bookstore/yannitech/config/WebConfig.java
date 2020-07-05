0ced986f5b147fed039b5ee311c7485948048f16
segwatlhe <segwatlhe@gmail.com>
2020-07-05 13:32:28 -0700

    ...

--- src/main/java/com/yannitech/bookstore/yannitech/config/WebConfig.java
+++ src/main/java/com/yannitech/bookstore/yannitech/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  
   @Override
   public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
     configurer.defaultContentType(MediaType.APPLICATION_XML);
