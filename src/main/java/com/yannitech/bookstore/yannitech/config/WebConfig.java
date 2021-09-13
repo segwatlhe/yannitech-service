44d25f5ca3575b9f20d168d916d9f3ef5229ed36
segwatlhe <segwatlhe@gmail.com>
2021-09-13 07:28:07 +0200

    commit

--- src/main/java/com/yannitech/bookstore/yannitech/config/WebConfig.java
+++ src/main/java/com/yannitech/bookstore/yannitech/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
   
   @Override
   public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
-    configurer.defaultContentType(MediaType.APPLICATION_XML);
+    configurer.defaultContentType(MediaType.APPLICATION_JSON);
   }
 }
